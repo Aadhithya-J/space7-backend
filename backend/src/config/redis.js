e0b4a52543f4a7dfdce02876274802deb50005d3
Aadhithya-J <aadhithya.jothikumar@outlook.com>
2026-03-11 11:15:12 +0530

    update redis connection for upstash

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,7 +1,8 @@
 const Redis = require('ioredis');
 
-const redis = new Redis(process.env.REDIS_URL);
-
+const redis = new Redis(process.env.REDIS_URL, {
+  maxRetriesPerRequest: null
+});
 redis.on('connect', () => {
     console.log('✅ Redis connected');
 });
